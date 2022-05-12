8ea31466e43b31d7d88a67102cfbf90ea080a181
Zean23 <100299651+Zean23@users.noreply.github.com>
2022-05-12 10:12:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
+function green () {
+    range = strip.range(0, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+}
 input.onButtonPressed(Button.A, function () {
-    red()
-    basic.pause(100)
+	
 })
+function yellow () {
+    range = strip.range(0, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    range = strip.range(2, 1)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+}
 function red () {
     range = strip.range(0, 1)
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
@@ -16,5 +31,10 @@ basic.showIcon(IconNames.Heart)
 strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 strip.setBrightness(20)
 basic.forever(function () {
-	
+    green()
+    basic.pause(100)
+    red()
+    basic.pause(100)
+    yellow()
+    basic.pause(100)
 })
